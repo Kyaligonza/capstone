94a7f40370366f6e0ce041113496445304836c13
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-04 10:58:10 -0700

    testing

--- app.py
+++ app.py
@@ -182,6 +182,108 @@ def create_app(test_config=None):
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad!"
 
+    # Error Handling
+    '''
+    Example error handling for unprocessable entity
+    '''
+
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+
+    '''
+    @Done implement error handlers using the @app.errorhandler(error) decorator
+        each error handler should return (with approprate messages):
+                jsonify({
+                        "success": False,
+                        "error": 404,
+                        "message": "resource not found"
+                        }), 404
+
+    '''
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+
+    '''
+    @Done implement error handler for 404
+        error handler should conform to general task above
+    '''
+
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Server Error"
+        }), 500
+
+
+    '''
+    @Done implement error handler for AuthError
+        error handler should conform to general task above
+    '''
+
+
+    @app.errorhandler(AuthError)
+    def auth_error(error):
+        return jsonify({
+            "success": False,
+            "error": error.status_code,
+            "message": error.error['description']
+        }), error.status_code
+
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": 'Method Not Allowed'
+        }), 405
+
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": 'Bad Request'
+        }), 400
+
+
+    @app.errorhandler(401)
+    def unauthorised(error):
+        return jsonify({
+            "success": False,
+            "error": 401,
+            "message": 'Unauthorised'
+        }), 401
+
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return jsonify({
+            "success": False,
+            "error": 403,
+            "message": 'Forbidden'
+        }), 403
+        
+
     return app
 
 app = create_app()
