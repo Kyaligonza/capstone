bf8af74f887dfd3f6b884d1b8450092e8f3055f3
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-09 16:05:21 -0700

    update

--- auth.py
+++ auth.py
@@ -41,8 +41,9 @@ def get_token_auth_header():
         return request.form['access_token']
     if 'access_token' in request.args:
         return request.args['access_token']
+    
     #auth = request.headers.get('Authorization', None)
-    auth = ({'Authorization':'Bearer {}'.format(access_token)}, None)
+    auth = ({'Authorization':'Bearer {}'.format(request.args['access_token'])}, None)
     # auth = request.headers.get('Authorization', None)
     if not auth:
         raise AuthError({
