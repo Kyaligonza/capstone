ea867707d3765d3437d63bf942feccc0fe4794a8
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-11 16:39:06 -0700

    requirements update

--- auth.py
+++ auth.py
@@ -2,7 +2,8 @@ import json
 from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
-from urllib.request import urlopen
+# from urllib.request import urlopen
+from six.moves.urllib.request import urlopen
 # import base64
 
 # import os 
