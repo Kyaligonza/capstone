cf2566f5e8854afc6757b7a283b5cbb3cdb7c015
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-13 14:53:44 -0700

    try again

--- auth.py
+++ auth.py
@@ -3,31 +3,10 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-# from six.moves.urllib.request import urlopen
-# import base64
-
-# import os 
-# SECRET_KEY=os.urandom(32)
-
-# Grabs the folder where the script runs.
-# basedir=os.path.abspath(os.path.dirname(__file__))
-
 
 AUTH0_DOMAIN = 'agent88.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'stars'
-
-# AUTH0_DOMAIN = 'agency99.us.auth0.com'
-# ALGORITHMS = ['RS256']
-# API_AUDIENCE = 'actors'
-
-CLIENT_ID = 'xFoG8R71EEFXmHIOKPxGLpdTQCG2iZVZ'
-CLIENT_SECRET = 'euZkCMgG5Kq2gBRiB4zgiIi8p1-eNOZ2RhIuBOuynF2mLVQdjpWOHC7DnS74ZR5_'
-# AuthError Exception
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
 
 
 class AuthError(Exception):
@@ -36,26 +15,9 @@ class AuthError(Exception):
         self.status_code = status_code
 
 
-# Auth Header
-
-'''
-@Done implement get_token_auth_header() method
-    it should attempt to get the header from the request
-    it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-    it should raise an AuthError if the header is malformed
-    return the token part of the header
-'''
-
-
 def get_token_auth_header():
     """Obtains the Access Token from the Authorization Header
     """
-    # if 'access_token' in request.form:
-    #     return request.form['access_token']
-    # if 'access_token' in request.args:
-    #     return request.args['access_token']
-    
     auth = request.headers.get('Authorization', None)
     if not auth:
         raise AuthError({
@@ -84,22 +46,6 @@ def get_token_auth_header():
 
     token = parts[1]
     return token
-#    raise Exception('Not Implemented')
-
-
-'''
-@Done implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
-
-        it should raise an AuthError if permissions
-        are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-        it should raise an AuthError if the requested permission
-        string is not in the payload permissions array
-        return true otherwise
-'''
 
 
 def check_permissions(permission, payload):
@@ -115,27 +61,42 @@ def check_permissions(permission, payload):
             'description': 'Permission not found.'
         }, 403)
     return True
-    # raise Exception('Not Implemented')
 
 
-'''
-@Done implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
 
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
 
-    !!NOTE urlopen has a common certificate error described here:
-    https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
 
 
 def verify_decode_jwt(token):
-
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
@@ -176,7 +137,8 @@ def verify_decode_jwt(token):
         except jwt.JWTClaimsError:
             raise AuthError({
                 'code': 'invalid_claims',
-                'description': 'Incorrect claims.Please, check the audience and issuer.'  # noqa
+                'description':
+                    'Incorrect claims. Please, check the audience and issuer.'
             }, 401)
         except Exception:
             raise AuthError({
@@ -185,22 +147,8 @@ def verify_decode_jwt(token):
             }, 400)
     raise AuthError({
         'code': 'invalid_header',
-                'description': 'Unable to find the appropriate key.'
+        'description': 'Unable to find the appropriate key.'
     }, 400)
-
-    # raise Exception('Not Implemented')
-
-'''
-@Done implement @requires_auth(permission) decorator method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        it should use the get_token_auth_header method to get the token
-        it should use the verify_decode_jwt method to decode the jwt
-        it should use the check_permissions method validate
-        claims and check the requested permission
-        return the decorator which passes the decoded payload
-        to the decorated method
-'''
 
 
 def requires_auth(permission=''):
@@ -210,7 +158,8 @@ def requires_auth(permission=''):
             token = get_token_auth_header()
             payload = verify_decode_jwt(token)
             check_permissions(permission, payload)
-            return f(payload, *args, **kwargs)
+            return f(*args, **kwargs)
 
         return wrapper
+
     return requires_auth_decorator
