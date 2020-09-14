6e36196ee2120061849d96937409ce77671c64f4
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-14 10:54:45 -0700

    update

--- auth.py
+++ auth.py
@@ -13,8 +13,8 @@ import os
 AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
 ALGORITHMS = os.environ.get('ALGORITHMS')
 API_AUDIENCE = os.environ.get('API_AUDIENCE')
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+# CLIENT_ID = os.environ.get('CLIENT_ID')
+# CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
 # AuthError Exception
 '''
