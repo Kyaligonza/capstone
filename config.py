7f89ed1384780a910ec9a4b019eda2dddee0b6bf
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-16 12:07:34 -0700

    config correction

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ auth0_config = {
     'AUTH0_DOMAIN' : 'agent88.us.auth0.com',
     'ALGORITHMS' : ['RS256'],
     'API_AUDIENCE' : 'stars',
-    'CLIENT_ID'='xFoG8R71EEFXmHIOKPxGLpdTQCG2iZVZ',
-    'CLIENT_SECRET='euZkCMgG5Kq2gBRiB4zgiIi8p1-eNOZ2RhIuBOuynF2mLVQdjpWOHC7DnS74ZR5_',
-    'AUTH0_CALLBACK_URL'='https://myapphsbk.herokuapp.com/'
+    'CLIENT_ID': 'xFoG8R71EEFXmHIOKPxGLpdTQCG2iZVZ',
+    'CLIENT_SECRET: 'euZkCMgG5Kq2gBRiB4zgiIi8p1-eNOZ2RhIuBOuynF2mLVQdjpWOHC7DnS74ZR5_'
+    # 'AUTH0_CALLBACK_URL'='https://myapphsbk.herokuapp.com/'
 }
