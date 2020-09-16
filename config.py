bc9a3610d67bb40558d404aa9d405adbeee154e6
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-16 12:02:05 -0700

    config correction

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 auth0_config = {
     'AUTH0_DOMAIN' : 'agent88.us.auth0.com',
     'ALGORITHMS' : ['RS256'],
-    'API_AUDIENCE' : 'stars'
-    'CLIENT_ID'='xFoG8R71EEFXmHIOKPxGLpdTQCG2iZVZ'
-    'CLIENT_SECRET='euZkCMgG5Kq2gBRiB4zgiIi8p1-eNOZ2RhIuBOuynF2mLVQdjpWOHC7DnS74ZR5_'
+    'API_AUDIENCE' : 'stars',
+    'CLIENT_ID'='xFoG8R71EEFXmHIOKPxGLpdTQCG2iZVZ',
+    'CLIENT_SECRET='euZkCMgG5Kq2gBRiB4zgiIi8p1-eNOZ2RhIuBOuynF2mLVQdjpWOHC7DnS74ZR5_',
     'AUTH0_CALLBACK_URL'='https://myapphsbk.herokuapp.com/'
 }
