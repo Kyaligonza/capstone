61610f17fc419692d20eeb2e1f9558be8a90a539
Kyaligonza <sarah.kyaligonza@gmail.com>
2020-09-22 12:22:48 -0700

    update

--- auth/auth.py
+++ auth/auth.py
@@ -80,7 +80,7 @@ def get_token_auth_header():
             'description': 'Authorization header is expected.'
         }, 401)
 
-    parts = auth.split('')
+    parts = auth.split(' ')
     if parts[0].lower() != 'bearer':
         raise AuthError({
             'code': 'invalid_header',
